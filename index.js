2b4105a30ef700090b5e873cc3ba6231c9f84c31
Lyndsay Vance <lyndsayvance@Lyndsays-MBP.attlocal.net>
2024-09-12 08:42:03 -0700

    Port change changing db_port usage

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app=express();
 
-const PORT = process.env.DB_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 require("dotenv").config();
 const client = require("./db/client");
