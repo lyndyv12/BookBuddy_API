fb7edc75b837bd884b32f82652606f5f4dae8d1f
Lyndsay Vance <lyndsayvance@Lyndsays-MBP.attlocal.net>
2024-09-12 07:13:51 -0700

    client.js changes

--- db/client.js
+++ db/client.js
@@ -1,11 +1,10 @@
-const pg=require("pg");
+const { Client } = require('pg');
+const connectionString = process.env.DATABASE_URL;
 
-const client = new pg.Client({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
+const db = new Client({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+
+  database: process.env.DB_NAME,
 });
-
-console.log(client);
-
-module.exports =client;
+module.exports = db;
