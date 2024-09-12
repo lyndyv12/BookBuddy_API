81436c0b8c64bf2a20e1da0876d1ccea18be36bc
Lyndsay Vance <lyndsayvance@Lyndsays-MBP.attlocal.net>
2024-09-12 07:38:20 -0700

    client.js changes v2

--- db/client.js
+++ db/client.js
@@ -4,7 +4,8 @@ const connectionString = process.env.DATABASE_URL;
 const db = new Client({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
   database: process.env.DB_NAME,
 });
 module.exports = db;
